542822d3c86ca2860a90cb1bf3c13cf07c790a6f
{自分の名前} <misaki.ishihara0004@gmail.com>
2022-05-19 20:30:28 +0900

    あ

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,25 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Particle from './src/particle'
+import Particle from './src/Particle'
 
 
 const Home: NextPage = () => 
 <>
-  <div className='main'>
-  <Particle/>
+<div className={styles.main}>
+    <header className={styles.header}>
+    <h1 className={styles.title}>CinemaBar</h1>
+    <nav>
+      <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+      </ul>
+    </nav>
+    </header>
+    <div className={styles.contents}>
+      Tell me your mood
+    </div>
+    <Particle/>
   </div>
 </>
 
